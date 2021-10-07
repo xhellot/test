44d00c2ca5f930a94f38caeccd0efdb429f1bc5d
Andrew Xu <xhellot2012@gmail.com>
2021-10-06 20:27:22 -0400

    Emily is watching!

--- ForEmily.py
+++ ForEmily.py
@@ -1,1 +1,14 @@
-print('hello, my name is Emily.')
+import pygame
+
+pygame.init()
+
+pygame.display.set_mode((800, 600))
+
+pygame.display.update()
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
